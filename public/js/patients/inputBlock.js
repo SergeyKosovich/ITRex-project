589e2e5abba8b0bf7f56b0c5b8cd977e2b38180c
SergeyKosovich <kosovichsergey322@gmail.com>
2021-08-13 16:42:23 +0300

    fix mistakes

--- public/js/patients/inputBlock.js
+++ public/js/patients/inputBlock.js
@@ -5,13 +5,13 @@ import namePost from './namePost.js';
 export default function inputPatient(addForm, patientStack, ws) {
   addForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const inner = e.target.elements.patient.value;
+    const nameInput = e.target.elements.patient.value;
     try {
-      await namePost(inner, e.target.ttlNumber.value);
+      await namePost(nameInput, e.target.ttlNumber.value);
     } catch (err) {
       console.log(err);
     }
-    const child = addDiv(patientStack, inner);
+    const child = addDiv(patientStack, nameInput);
     if (e.target.ttl.checked) {
       setTimeout(async () => {
         patientStack.removeChild(child);
@@ -19,6 +19,6 @@ export default function inputPatient(addForm, patientStack, ws) {
     }
     e.target.elements.patient.value = '';
     e.target.ttlNumber.value = '';
-    ws.send(JSON.stringify({ name: inner, event: 'addUser' }));
+    ws.send(JSON.stringify({ name: nameInput, event: 'addUser' }));
   });
 }
